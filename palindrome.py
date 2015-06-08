ccfd708655e909fd7b8ce86ff1035db7a376e17d
qq53 <1730806439@qq.com>
2015-06-07 21:55:11 -0400

    sorted

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def is_palindrome(n):
+	return str(n) == str(n)[::-1]
+
+print(list(filter(is_palindrome, range(1,100))))
---
+++ sorted2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def by_name(t):
+	return t[1]
+
+L = [('Bob',75),('Adam',92),('Bart',66),('Lisa',88)]
+
+print(sorted(L, key=by_name))
