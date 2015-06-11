64057eab8dc2984f08b191a76631bcf210c2b773
qq53 <sen1993@xiyoulinux.org>
2015-06-11 10:08:22 -0400

    bmpinfo

--- bmpinfo.py
+++ bmpinfo.py
@@ -2,9 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import struct
+from enum import Enum, unique
+
+@unique
+class BMP(Enum):
+	SIGN = 0
+	SIZE = 1
+	WIDTH = 5
+	HEIGHT = 6
+	COLOR = 8
 
 with open('struct.bmp', 'rb') as f:
 	h = f.read(30)
-sh = struct.unpack('<ccIIIIIIHH', h)
-if h[:2] == 'BM':
-	print('yes')
+sh = struct.unpack('<2sIIIIIIHH', h)
+if sh[BMP.SIGN.value] == b'BM':
+	print('is bmp')
+	print('size: %s x %s' % (sh[BMP.WIDTH.value], sh[BMP.HEIGHT.value]))
+	print('color sum: %d' % sh[BMP.COLOR.value])
+else:
+	print('not bmp')
