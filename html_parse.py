86c2aac5e857d202a077b481c72888108ccc79d0
qq53 <sen1993@xiyoulinux.org>
2015-06-11 16:32:34 -0400

    urllib

---
+++ html_parse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.4
+# -*- coding: utf-8 -*-
+
+import re
+from urllib import request
+import html
+with request.urlopen('http://www.python.org/events/python-events/') as f:
+	if f.status == 200:
+		data = f.read().decode('utf8')
+		recent = re.findall(r'<ul class="list-recent-events menu">(.+?)</ul>',data,re.S)
+		time = re.findall(r'<time.*?>(.+?)</time>',recent[0],re.S)
+		time = list(map(html.unescape, time))
+		time = list(map(lambda x:re.findall(r'(.+?) <',x)[0],time))
+		event = re.findall(r'<h3 class="event-title">(.+?)</h3>',recent[0],re.S)
+		event = list(map(lambda x:re.findall(r'<a.+?>(.+)</a>',x)[0],event))
+		place = re.findall(r'<span class="event-location">(.+?)</span>',recent[0],re.S)
+		print('---Python Events Calendar [Upcoming Events]---\n')
+		for i in range(0,len(time)):
+			print('Date: ' + time[i])
+			print('Event: ' + event[i])
+			print('Location: ' + place[i] + '\n')
